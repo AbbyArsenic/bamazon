b60488b42be1a5c19f7331fa9d8cfb386d5a3351
Abby Johnson <brit13@gmail.com>
2017-10-11 16:46:01 -0700

    basic app fully functional

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # bamazon
 Node.js and MySQL HW Week12
+
+## Challenge 1
+*Step 1*
+Open command line or terminal and enter "node bamazonCustomer.js" to start the app
+![step 1](/basicImages/Step1.png)
+
+*Step 2*
+A list of available products is displayed. Where prompted, enter the item id of the product you wish to purchase. 
+![step 2](/basicImages/Step2.png)
+
+*Step 3*
+After selecting the desired product, enter the quantity you wish to purchase. 
+![step 3](/basicImages/Step3.png)
+
+*Step 4*
+If there is sufficient quantity in stock, the app will update inventory in the database and calculate your order total. 
+![step 4](/basicImages/Step4.png)
+
+*Insufficient Stock*
+If selected quantity is greater than available inventory, you will be notified of insufficient stock and prompted to begin your search again. 
+![Insufficient Stock](/basicImages/Error.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,24 +26,41 @@ connection.query('SELECT * FROM `products`', function (err, results, fields) {
   for (var i=0; i<results.length; i++) {
     console.log(results[i].item_id + " " + results[i].product_name + " [" + results[i].price + "]");
   }
+  // Prompt user to select a product and enter desired quantity
   function buyPrompt() {
     inquirer.prompt( [{
-      name: "buyItem",
+      name: "itemId",
       type: "input",
       message: "Enter the id of the product you'd like to buy."
     }, {
       name: "quantity",
       type: "input",
-      message: "Enter the quantity to purchase."
+      message: "How many would you like to purchase?"
     }]).then(function(answer) {
       for (var i=0; i<results.length; i++) {
-        if (results[i].item_id === parseInt(answer.buyItem)) {
+        if (results[i].item_id === parseInt(answer.itemId)) {
+          // If order quantity is too high, notify user of insufficient stock
           if (results[i].stock_quantity < parseInt(answer.quantity)) {
             console.log("Insufficient stock!");
             buyPrompt();
           } else {
-            console.log("You have successfully purchased " + answer.quantity + " " + results[i].product_name);
-            console.log("Your order total is " + parseInt(answer.quantity)*results[i].price);
+            // Calculate order total and remaining stock
+            var total = parseFloat(answer.quantity*results[i].price).toFixed(2);
+            var newStock = results[i].stock_quantity - answer.quantity;
+
+            // Notify user of successful purchase
+            console.log("You have purchased " + answer.quantity + " " + results[i].product_name);
+            console.log("Your order total is " + total);
+
+            // Construct query to update stock
+            var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.itemId
+            connection.query(updateStock, function(err, result) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(result.affectedRows + " product updated");
+              }
+            });
           }
         }
       }
